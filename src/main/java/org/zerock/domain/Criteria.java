3ea9fa31bd48ddfa692df191aa7d21af9c50ee2d
pigm6080 <pigm6080@gmail.com>
2024-03-15 00:33:52 +0900

    sql 문 업로드

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -1,5 +1,9 @@
 package org.zerock.domain;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import org.apache.wml.WMLStrongElement;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +15,9 @@ public class Criteria {
 	
 	private int pageNum;
 	private int amount;
+	
+	private String type;
+	private String keyword;
 
 	public Criteria() {
 		this(1,10);
@@ -18,5 +25,10 @@ public class Criteria {
 	public Criteria(int pageNum, int amount) {
 		this.pageNum = pageNum;
 		this.amount = amount;
+	}
+	
+	public String[] getTypeArr() {
+		 
+		return type == null? new String[] {} : type.split("");
 	}
 }
